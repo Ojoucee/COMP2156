066dfe6d722162ff8bd7bcde5cfbbce6c3d16a1e
Jay Chris Areola <JayChris.Areola@georgebrown.ca>
2023-02-12 14:12:46 -0500

    changed

---
+++ build/precache-manifest.8e2ae1f78ec5f69f04b35d1891800e04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29de1d9f593e87fa4267",
+    "url": "/Ojoucee/COMP2156/static/css/main.50a5ced5.chunk.css"
+  },
+  {
+    "revision": "29de1d9f593e87fa4267",
+    "url": "/Ojoucee/COMP2156/static/js/main.29de1d9f.chunk.js"
+  },
+  {
+    "revision": "42c14c960a6e803a3f78",
+    "url": "/Ojoucee/COMP2156/static/js/runtime~main.42c14c96.js"
+  },
+  {
+    "revision": "64d6decb088646398d8f",
+    "url": "/Ojoucee/COMP2156/static/js/2.64d6decb.chunk.js"
+  },
+  {
+    "revision": "a2cef8278d5ee576c1591a9854f76bc8",
+    "url": "/Ojoucee/COMP2156/index.html"
+  }
+];
